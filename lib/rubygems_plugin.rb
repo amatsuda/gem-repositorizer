d32a9e21127c9602f46fd506b4f533799d974e96
Akira Matsuda <ronnie@dio.jp>
2015-05-20 10:51:28 +0900

    Don't init if already inside a git repo

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -4,7 +4,7 @@ Gem.post_install do |installer|
   gem_dir = installer.respond_to?(:gem_dir) ? installer.gem_dir : File.expand_path(File.join(installer.gem_home, 'gems', installer.spec.full_name))
   lib_dir = File.join gem_dir, 'lib'
   if Dir.exist? lib_dir
-    `cd #{lib_dir} && git init && git add -A && git commit -m 'Initial commit by gem-repositorizer'`
+    `cd #{lib_dir} && ! git rev-parse --is-inside-work-tree 2> /dev/null && git init && git add -A && git commit -m 'Initial commit by gem-repositorizer'`
   end
   true
 end
